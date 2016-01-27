6c4e0301e96ce65271e821026a1a937cc894ec41
Aidan Feldman <aidan.feldman@gsa.gov>
2016-01-26 23:22:46 -0500

    include PrivateEye

--- assets/js/18f-guide.js
+++ assets/js/18f-guide.js
@@ -2,4 +2,16 @@ $(function() {
   $('.sidebar-nav').each(function() {
     accordion($(this));
   });
+
+  // https://github.com/18F/private-eye#usage
+  PrivateEye({
+    ignoreUrls: [
+      'https://docs.google.com',
+      'https://drive.google.com',
+      'https://github.com/18F/Accessibility_Reviews',
+      'https://github.com/18F/DevOps',
+      'https://github.com/18F/handbook',
+      'https://github.com/18F/writing-lab'
+    ]
+  });
 });
