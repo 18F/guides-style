501c9198d02c8e468099930071a22f038ddb5c20
Mike Bland <michael.bland@gsa.gov>
2016-02-10 13:37:24 -0500

    Implement flat URL namespacing

--- lib/guides_style_18f/generator.rb
+++ lib/guides_style_18f/generator.rb
@@ -11,6 +11,20 @@ module GuidesStyle18F
       site.collections['pages'].docs.each do |page|
         page.data['breadcrumbs'] = breadcrumbs[page.url]
       end
+      flatten_url_namespace(site) if site.config['flat_namespace']
+    end
+
+    def flatten_url_namespace(site)
+      site.collections['pages'].docs.each do |page|
+        page.data['permalink'] = flat_url(page.url)
+        (page.data['breadcrumbs'] || []).each do |crumb|
+        crumb['url'] = flat_url(crumb['url'])
+        end
+      end
+    end
+
+    def flat_url(url)
+      File.join('', File.basename(url), '')
     end
   end
 end
