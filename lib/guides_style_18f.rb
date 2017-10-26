fe045073a0d450315dd9776609162af9e3ed781e
Aidan Feldman <aidan.feldman@gsa.gov>
2017-10-26 15:15:18 -0400

    convert to a gem-based theme
    This fixes the theme to work for Jekyll 3.2+, and is easier to override.
    
    https://jekyllrb.com/docs/themes/#creating-a-gem-based-theme

--- lib/guides_style_18f.rb
+++ lib/guides_style_18f.rb
@@ -1,13 +1,9 @@
 # @author Mike Bland (michael.bland@gsa.gov)
 
-require 'guides_style_18f/assets'
 require 'guides_style_18f/breadcrumbs'
 require 'guides_style_18f/generator'
-require 'guides_style_18f/includes'
-require 'guides_style_18f/layouts'
 require 'guides_style_18f/navigation'
 require 'guides_style_18f/repository'
-require 'guides_style_18f/sass'
 require 'guides_style_18f/tags'
 require 'guides_style_18f/update'
 require 'guides_style_18f/version'
--- lib/guides_style_18f/generator.rb
+++ lib/guides_style_18f/generator.rb
@@ -1,9 +1,7 @@
 # @author Mike Bland (michael.bland@gsa.gov)
 
-require_relative './assets'
 require_relative './breadcrumbs'
 require_relative './generated_pages'
-require_relative './layouts'
 require_relative './namespace_flattener'
 
 require 'jekyll'
@@ -11,8 +9,6 @@ require 'jekyll'
 module GuidesStyle18F
   class Generator < ::Jekyll::Generator
     def generate(site)
-      Layouts.register(site)
-      Assets.copy_to_site(site)
       GeneratedPages.generate_pages_from_navigation_data(site)
       pages = site.collections['pages']
       docs = (pages.nil? ? [] : pages.docs) + site.pages
